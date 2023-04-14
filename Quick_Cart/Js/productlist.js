eaca202c3cacde08e928ede11b988fb007845629
Sudhir <sudhiradhe001@gmail.com>
2023-04-14 17:36:04 +0530

    New Pages Added

--- Quick_Cart/Js/productlist.js
+++ Quick_Cart/Js/productlist.js
@@ -5,21 +5,16 @@ const type=document.getElementById("type");
 const offers=document.getElementById("offers");
 brnd.addEventListener("click",()=>{
     document.getElementById("drop-block1").classList.toggle("drop-block-on-off");
-    // console.log("HI");
 });
 price.addEventListener("click",()=>{
     document.getElementById("drop-block2").classList.toggle("drop-block-on-off");
-    // console.log("HI");
 });
 colr.addEventListener("click",()=>{
     document.getElementById("drop-block3").classList.toggle("drop-block-on-off");
-    // console.log("HI");
 });
 type.addEventListener("click",()=>{
     document.getElementById("drop-block4").classList.toggle("drop-block-on-off");
-    // console.log("HI");
 });
 offers.addEventListener("click",()=>{
     document.getElementById("drop-block5").classList.toggle("drop-block-on-off");
-    // console.log("HI");
 });
